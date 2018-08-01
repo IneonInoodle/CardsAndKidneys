6fc1ea862941419ed78c905a5230a61c1d009292
Cas <anhammami@stud.mediadesign.de>
2018-08-01 15:09:03 +0200

    Glow Effect Geadded

---
+++ Assets/Prefabs/Effects/CardsEffect_Color_Editor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsEffect_Color_Editor : MonoBehaviour {
+
+    public Material MatObject1;
+    public Color color1 = Color.black;
+
+    public Material MatObject2;
+    public Color color2 = Color.black;
+
+    public Material MatObject3;
+    public Color color3 = Color.black;
+
+    public Material MatObject4;
+    public Color color4 = Color.black;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        color1 = Color.red;
+	}
+}
