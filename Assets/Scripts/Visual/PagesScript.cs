1a1f4de02a929e308bc1a63388703c597a249550
Cas <anhammami@stud.mediadesign.de>
2018-08-15 15:16:18 +0200

    TutoundMain

---
+++ Assets/Scripts/Visual/PagesScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PagesScript : MonoBehaviour {
+
+    [SerializeField]
+    public int CurrentImageNr = 1;
+    public Image MainImage;
+    public Sprite[] Images;
+    void Awake ()
+    {
+        MainImage.sprite = Images[0];
+    }
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+    public void NextImage()
+    {
+        MainImage.sprite = Images[CurrentImageNr + 1];
+        CurrentImageNr++;
+    }
+    public void PreviousImage()
+    {
+        MainImage.sprite = Images[CurrentImageNr - 1];
+        CurrentImageNr--;
+    }
+}
